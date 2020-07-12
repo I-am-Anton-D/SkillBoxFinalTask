24d182d7f7c49c4b56bdbc3d9752153c2fba38ae
I-am-Anton-D <motohd@yandex.ru>
2020-07-12 18:25:28 +0300

    Like/Dislike implementation

--- src/main/java/main/controller/ApiPostController.java
+++ src/main/java/main/controller/ApiPostController.java
@@ -1,8 +1,11 @@
 package main.controller;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashSet;
@@ -12,6 +15,10 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import javax.servlet.http.HttpServletRequest;
 import main.model.ModerationStatus;
+import main.model.PostComments;
+import main.model.PostCommentsRepository;
+import main.model.PostVotes;
+import main.model.PostVotesRepository;
 import main.model.Posts;
 import main.model.PostsRepository;
 import main.model.Tag2Post;
@@ -25,6 +32,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +46,6 @@ public class ApiPostController {
     final static String YESTERDAY = "Вчера, ";
     final static int ANNOUNCE_LENGTH = 200;
 
-
     private JSONObject response, request = null;
     private final JSONParser parser = new JSONParser();
     @Autowired
@@ -49,6 +56,77 @@ public class ApiPostController {
     private UsersRepository usersRepository;
     @Autowired
     private Tag2PostRepository tag2PostRepository;
+    @Autowired
+    private PostCommentsRepository postCommentsRepository;
+    @Autowired
+    private PostVotesRepository postVotesRepository;
+
+
+    @PostMapping("/api/post/dislike")
+    public String dislikePost(@RequestBody String body, HttpServletRequest httpRequest) throws ParseException {
+        //TODO Need some refactoring
+        response = new JSONObject();
+        if (ApiAuthController.sessions.containsKey(httpRequest.getSession().getId())) {
+            JSONObject request = (JSONObject) parser.parse(body);
+            int postId = (int) ((long) request.get("post_id"));
+            int userID = ApiAuthController.sessions.get((httpRequest.getSession().getId()));
+            if (!checkHasLike(postId, userID, -1)) {
+                PostVotes postVote = new PostVotes();
+                postVote.setPostId(postId);
+                postVote.setUserId(userID);
+                postVote.setTime(new Date());
+                postVote.setValue((byte) -1);
+                postVotesRepository.save(postVote);
+                response.put("result", true);
+            } else {
+                response.put("result", false);
+            }
+        } else {
+            response.put("result", false);
+        }
+        return response.toJSONString();
+    }
+
+
+
+    @PostMapping("/api/post/like")
+    public String likePost(@RequestBody String body, HttpServletRequest httpRequest) throws ParseException {
+        //TODO Need some refactoring
+        response = new JSONObject();
+        if (ApiAuthController.sessions.containsKey(httpRequest.getSession().getId())) {
+            JSONObject request = (JSONObject) parser.parse(body);
+            int postId = (int) ((long) request.get("post_id"));
+            int userID = ApiAuthController.sessions.get((httpRequest.getSession().getId()));
+            if (!checkHasLike(postId, userID, 1)) {
+                PostVotes postVote = new PostVotes();
+                postVote.setPostId(postId);
+                postVote.setUserId(userID);
+                postVote.setTime(new Date());
+                postVote.setValue((byte) 1);
+                postVotesRepository.save(postVote);
+                response.put("result", true);
+            } else {
+                response.put("result", false);
+            }
+        } else {
+            response.put("result", false);
+        }
+        return response.toJSONString();
+    }
+
+
+    @GetMapping("/api/post/{id}")
+    public String getPost(@PathVariable int id) {
+        response = new JSONObject();
+        response = transformPostToJsonObject(postsRepository.findById(id).get());
+        Posts post = postsRepository.findById(id).get();
+        post.setViewCount(post.getViewCount() + 1);
+        postsRepository.save(post);
+        response.put("comments", getComments(id));
+        response.put("tags", getTags(id));
+        return response.toJSONString();
+    }
+
 
     @GetMapping("/api/post/search")
     public String search(HttpServletRequest request) {
@@ -56,22 +134,17 @@ public class ApiPostController {
         int offset = Integer.parseInt(request.getParameter("offset"));
         int limit = Integer.parseInt(request.getParameter("limit"));
         String query = request.getParameter("query");
-//        Can not make empty query, frontend error
-//        List<Posts> allPost = getVisiblePost();
-//        if (query.length()==0) {
-//            response =  transformListToJsonString(allPost.stream().skip(offset).limit(limit).collect(Collectors.toList()));
-//        }
         List<Posts> searchedPost = getVisiblePost().stream()
-            .filter(p->p.getText().contains(query) || p.getTitle().contains(query))
+            .filter(p -> p.getText().contains(query) || p.getTitle().contains(query))
             .collect(Collectors.toList());
-        if (searchedPost.size()==0) {
-            response.put("result",false);
+        if (searchedPost.size() == 0) {
+            response.put("result", false);
         } else {
-            response =  transformListToJsonString(searchedPost.stream().skip(offset).limit(limit).collect(Collectors.toList()));
+            response = transformListToJsonObject(searchedPost.stream().skip(offset)
+                .limit(limit).collect(Collectors.toList()));
         }
         return response.toJSONString();
     }
-
 
     @GetMapping("/api/post")
     public String getPosts(HttpServletRequest request) {
@@ -90,14 +163,17 @@ public class ApiPostController {
         }
 
         if (mode.equals("best")) {
-            //TODO best
+            sortedList = sortedList.stream().sorted(Comparator.comparing(posts -> getLikes(posts.getId(),1))).collect(
+                Collectors.toList());
+            Collections.reverse(sortedList);
         }
 
         if (mode.equals("early")) {
             sortedList = sortedList.stream().sorted(Comparator.comparing(Posts::getTime))
                 .collect(Collectors.toList());
         }
-        response = transformListToJsonString(sortedList.stream().skip(offset).limit(limit).collect(Collectors.toList()));
+        response = transformListToJsonObject(sortedList.stream().skip(offset)
+            .limit(limit).collect(Collectors.toList()));
         return response.toJSONString();
     }
 
@@ -155,7 +231,7 @@ public class ApiPostController {
     public String tag(HttpServletRequest request) {
         response = new JSONObject();
         Set<Integer> visibleIds = new HashSet<>();
-        for (Posts post: getVisiblePost()) {
+        for (Posts post : getVisiblePost()) {
             visibleIds.add(post.getId());
         }
         Iterable<Tags> tags = tagsRepository.findAll();
@@ -163,7 +239,7 @@ public class ApiPostController {
         for (Tags tag : tags) {
             JSONObject tagObject = new JSONObject();
             tagObject.put("name", tag.getName());
-            tagObject.put("weight", calculateTagWeight(tag.getId(),visibleIds));
+            tagObject.put("weight", calculateTagWeight(tag.getId(), visibleIds));
             tagsArray.add(tagObject);
         }
 
@@ -171,7 +247,7 @@ public class ApiPostController {
             response.put("tags", tagsArray);
         } else {
             String query = request.getParameter("query");
-            for (int i = 0; i <tagsArray.size(); i++) {
+            for (int i = 0; i < tagsArray.size(); i++) {
                 JSONObject jsonTag = (JSONObject) tagsArray.get(i);
                 if (query.equals(jsonTag.get("name"))) {
                     response.put("name", jsonTag.get("name"));
@@ -210,37 +286,96 @@ public class ApiPostController {
         double totalCount = visibleIds.size();
         int frequencyTag = 0;
         Iterable<Tag2Post> tag2Posts = tag2PostRepository.findAll();
-        for (Tag2Post tag: tag2Posts) {
+        for (Tag2Post tag : tag2Posts) {
             if (visibleIds.contains(tag.getPostId()) && tag.getTagId() == tagId) {
                 frequencyTag++;
             }
         }
-        double weight = frequencyTag/totalCount;
-        return Double.parseDouble(new DecimalFormat("#.##").format(weight).replace(",","."));
+        double weight = frequencyTag / totalCount;
+        return Double.parseDouble(new DecimalFormat("#.##").format(weight).replace(",", "."));
     }
 
-    public JSONObject transformListToJsonString(List<Posts> posts) {
+    public JSONObject transformListToJsonObject(List<Posts> posts) {
         response = new JSONObject();
         JSONArray jsonArray = new JSONArray();
-        for (Posts post : posts) {
-            JSONObject jsonPost = new JSONObject();
-            jsonPost.put("id", post.getId());
-            jsonPost.put("time", formatTime(post.getTime()));
-            JSONObject jsonUser = new JSONObject();
-            jsonUser.put("id", post.getUserId());
-            jsonUser.put("name", usersRepository.findById(post.getUserId()).get().getName());
-            jsonPost.put("user", jsonUser);
-            jsonPost.put("title", post.getTitle());
-            jsonPost.put("announce", post.getText().replaceAll("\\<[^>]*>","").replaceAll("&nbsp;"," ").substring(0,ANNOUNCE_LENGTH));
-            jsonPost.put("likeCount", 0); //TODO LikeCount
-            jsonPost.put("dislikeCount", 0); //TODO dislikeCount
-            jsonPost.put("commentCount", 0); //TODO commentCount
-            jsonPost.put("viewCount", post.getViewCount());
-            jsonArray.add(jsonPost);
-
-        }
+        posts.forEach(post -> jsonArray.add(transformPostToJsonObject(post)));
         response.put("count", posts.size());
         response.put("posts", jsonArray);
         return response;
     }
+
+    public JSONObject transformPostToJsonObject(Posts post) {
+        JSONObject jsonPost = new JSONObject();
+        jsonPost.put("id", post.getId());
+        jsonPost.put("time", formatTime(post.getTime()));
+        JSONObject jsonUser = new JSONObject();
+        jsonUser.put("id", post.getUserId());
+        jsonUser.put("name", usersRepository.findById(post.getUserId()).get().getName());
+        jsonPost.put("user", jsonUser);
+        jsonPost.put("title", post.getTitle());
+        jsonPost.put("announce", post.getText().replaceAll("\\<[^>]*>", "").replaceAll("&nbsp;", " ")
+            .substring(0, ANNOUNCE_LENGTH));
+        jsonPost.put("text", post.getText());
+        jsonPost.put("likeCount", getLikes(post.getId(),1));
+        jsonPost.put("dislikeCount", getLikes(post.getId(),-1));
+        jsonPost.put("commentCount", 0); //TODO commentCount
+        jsonPost.put("viewCount", post.getViewCount());
+        return jsonPost;
+    }
+
+    public boolean checkHasLike(int postId, int userId, int value) {
+        Iterable<PostVotes> postVotes = postVotesRepository.findAll();
+        for (PostVotes v : postVotes) {
+            if (v.getUserId() == userId && v.getPostId() == postId && v.getValue()==value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getLikes(int postId, int value) {
+        int count = 0;
+        Iterable<PostVotes> postVotes = postVotesRepository.findAll();
+        for (PostVotes v : postVotes) {
+            if (v.getValue() == value && v.getPostId() == postId) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public JSONArray getComments(int postId) {
+        JSONArray arrayOfcomments = new JSONArray();
+        Iterable<PostComments> comments = postCommentsRepository.findAll();
+        comments.forEach(c -> {
+            if (c.getPostId() == postId) {
+                JSONObject jsonComment = new JSONObject();
+                jsonComment.put("id", c.getId());
+                jsonComment.put("time", c.getTime());
+                jsonComment.put("text", c.getText());
+                JSONObject jsonUser = new JSONObject();
+                jsonUser.put("id", c.getUserId());
+                jsonUser.put("name", usersRepository.findById(c.getUserId()).get().getName());
+                jsonUser.put("photo", usersRepository.findById(c.getUserId()).get().getPhoto());
+                jsonComment.put("user", jsonUser);
+                arrayOfcomments.add(jsonComment);
+            }
+        });
+
+        return arrayOfcomments;
+    }
+
+    private JSONArray getTags(int id) {
+        JSONArray arrayOfTags = new JSONArray();
+        Iterable<Tag2Post> tags = tag2PostRepository.findAll();
+        tags.forEach(tag -> {
+            if (tag.getPostId() == id) {
+                arrayOfTags.add(tagsRepository.findById(tag.getTagId()).get().getName());
+            }
+        });
+        return arrayOfTags;
+    }
+
+
 }
---
+++ src/main/java/main/model/PostCommentsRepository.java
@@ -0,0 +1,7 @@
+package main.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostCommentsRepository extends CrudRepository<PostComments, Integer> {
+
+}
